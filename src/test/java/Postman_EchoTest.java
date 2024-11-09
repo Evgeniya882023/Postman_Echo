26195fa76f230ca9a73c92e786130a50928638cd
Evgeniya Malashenko <malashenko_genya@mail.ru>
2024-11-10 01:55:21 +0300

    First commit

---
+++ src/test/java/Postman_EchoTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class Postman_EchoTest {
+    @Test
+    void shouldTestSomeData() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Hello,World") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("some data", equalTo("Hello,World"));/* --> ваша проверка здесь <-- */
+    }
+}
